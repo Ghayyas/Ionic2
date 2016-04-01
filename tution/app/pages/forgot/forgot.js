5d981b234b6b98b101d081ab92834884c121746e
Ghayyas <ghayyasmubashir@gmail.com>
2016-04-01 16:58:58 +0500

    adding sign up and forgot page

---
+++ tution/app/pages/forgot/forgot.js
@@ -0,0 +1,10 @@
+import {Page} from "ionic-angular";
+
+@Page({
+    templateUrl: 'build/pages/forgot/forgot.html'
+})
+export class forgot{
+    constructor(){
+        
+    }
+}
---
+++ tution/app/pages/signup/signup.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/signup/signup.html'
+})
+export class signup {
+  constructor() {
+
+  }
+}
